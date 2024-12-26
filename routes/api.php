07026b5342f6d169868d6097c9587fe9a4cd1e73
Ronald Niz <ronaldalexisniznunez@gmail.com>
2024-12-26 16:15:05 -0300

    Codigo de rutas optimizado api.php

--- routes/api.php
+++ routes/api.php
@@ -9,34 +9,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
-
-    // Grupo de Rutas Pertenecientes a companias
-    Route::prefix('companias')->group(function () {
-
-        // Obterner todas las companias
-        Route::get('/', [CompaniaController::class, 'obtenerCompanias']);
-
-        // Obterner todas las companias con paginacion
-        Route::get('/obtenerConPaginacion', [CompaniaController::class, 'obtenerConPaginacion']);
-
-        // Obterner Compania por parametro
-        Route::get('/{compania}', [CompaniaController::class, 'obtenerCompania']);
+Route::prefix('v1/companias')
+    ->controller(CompaniaController::class)
+    ->group(function () {
+        Route::get('/', 'obtenerCompanias');
+        Route::get('/obtenerConPaginacion', 'obtenerConPaginacion');
+        Route::get('/{compania}', 'obtenerCompania');
     });
 
-    // Grupo de Rutas Pertenecientes a Personales
-    Route::prefix('personales')->group(function () {
-
-        // Obterner todas las Perosnales
-        Route::get('/', [PersonalController::class, 'obtenerPersonales']);
-
-        // Obterner todas las companias con paginacion
-        Route::get('/obtenerConPaginacion', [PersonalController::class, 'obtenerConPaginacion']);
-
-        // Obterner Personal por Documento
-        Route::get('/obtenerPorDocumento/{documeto}', [PersonalController::class, 'obtenerPorDocumento']);
-
-        // Obterner Personal por codigo
-        Route::get('/obtenerPorCodigo/{codigo}', [PersonalController::class, 'obtenerPorCodigo']);
+Route::prefix('v1/personales')
+    ->controller(PersonalController::class)
+    ->group(function () {
+        Route::get('/', 'obtenerPersonales');
+        Route::get('/obtenerConPaginacion', 'obtenerConPaginacion');
+        Route::get('/obtenerPorDocumento/{documeto}', 'obtenerPorDocumento');
+        Route::get('/obtenerPorCodigo/{codigo}', 'obtenerPorCodigo');
     });
-});
